abf621107b1f42949effe1b71d4c5ce4ad60a03b
Jochem Hoorneman <j.d.hoorneman@student.tudelft.nl>
2021-04-06 19:15:56 +0200

    moved method that reads from csv to election class and made voters a class property of election class

--- election.py
+++ election.py
@@ -1,21 +1,24 @@
 class Election:
     # does instant runoff election
 
-    def __init__(self, routes: list):
+    def __init__(self, routes: list, voters: list):
         self.routes = routes
+        self.voters = voters
 
-    def rank_routes(self, voters: list) -> dict:
+    def load_votes(self):
+        pass
+
+    def rank_routes(self) -> dict:
         # counts the votes for each route
         prel_result = {route: 0 for route in self.routes}
-        for voter in voters:
+        for voter in self.voters:
             prel_result[voter.ranking[0]] += 1
 
         return prel_result
 
-    @staticmethod
-    def find_last_place(prel_result: dict, voters: list) -> str:
+    def find_last_place(self, prel_result: dict) -> str:
         # finds out which route is the least voted on (and handles last place ties)
-        least_votes = len(voters)+1
+        least_votes = len(self.voters)+1
         last_places = []
         for route in prel_result:
             if prel_result[route] < least_votes:
@@ -31,21 +34,21 @@ class Election:
 
         return last_place
 
-    def prune_last_place(self, last_place: str, voters: list) -> dict:
+    def prune_last_place(self, last_place: str) -> dict:
         # prunes the route with the least votes from the routes list and from all the voters rankings
         self.routes.remove(last_place)
-        for voter in voters:
+        for voter in self.voters:
             voter.ranking.remove(last_place)
 
-        return self.rank_routes(voters)
+        return self.rank_routes()
 
-    def find_winner(self, voters) -> str:
+    def find_winner(self) -> str:
         # declares which route won the vote by using instant run-off voting
-        prel_result = self.rank_routes(voters)
+        prel_result = self.rank_routes()
 
         while len(prel_result) > 1:
-            last_place = self.find_last_place(prel_result, voters)
-            prel_result = self.prune_last_place(last_place, voters)
+            last_place = self.find_last_place(prel_result)
+            prel_result = self.prune_last_place(last_place)
 
         winner = list(prel_result.keys())[0]
         print(winner + ' got the most votes!')
--- voter.py
+++ voter.py
@@ -1,9 +1,5 @@
 class Voter:
-    # iemand die stemt
+    # someone casting a vote
     def __init__(self, name: str, ranking: list):
         self.name = name
         self.ranking = ranking
-
-    def from_file(self, filename: str):
-        # leest shit uit csv bestand en maakt een voter aan
-        pass
