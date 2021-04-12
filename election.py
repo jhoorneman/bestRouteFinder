5bf4d8b3f949c8102daeea9fb647d5772dca8fdb
Ike Mulder <ikemulder@hotmail.com>
2021-04-13 00:12:25 +0200

    Implemented tideman alternative, tiebreaking based on lowest copeland score.

--- election.py
+++ election.py
@@ -1,4 +1,4 @@
-from typing import Optional
+from typing import Optional, Dict, Set
 
 import copy
 
@@ -54,7 +54,7 @@ class ElectionResult:
     def find_last_place(self) -> str:
         # finds out which route is the least voted on (and handles last place ties)
         prel_result = self.rank_routes()
-        least_votes = len(self.voters)+1
+        least_votes = len(self.voters) + 1
         last_places = []
         for route in prel_result:
             if prel_result[route] < least_votes:
@@ -64,12 +64,19 @@ class ElectionResult:
                 last_places += [route]
 
         if len(last_places) > 1:
-            # TODO: THIS IS FLAWED
+            # Try to break ties by comparing copeland scores https://electowiki.org/wiki/Copeland%27s_method
+            scores = self.get_copeland_score()
+            last_place_scores = {route: scores[route] for route in last_places}
+            min_score = min(last_place_scores.values())
+            last_places = [route for route, score in last_place_scores.items() if score == min_score]
+
+        if len(last_places) > 1:
+            # Failed to break ties; announce and complain
             print('WRONGLY returning alphabetically last {} from tied last place between {}'.format(
-                last_places[-1],
+                sorted(last_places)[-1],
                 last_places
             ))
-            last_place = last_places[-1]
+            last_place = sorted(last_places)[-1]
         else:
             last_place = last_places[0]
 
@@ -90,8 +97,22 @@ class ElectionResult:
         # TODO: improve this
         return str(self.rank_routes())
 
+    def get_pairwise_votes(self) -> Dict[str, Dict[str, int]]:
+        # returns a dict r, for which r[route1][route2] = number of voters voting for route 1 in a match against route2
+        return {route1: {
+            route2: len([voter for voter in self.voters if voter.prefers_over(route1, route2)])
+            for route2 in self.routes
+        } for route1 in self.routes}
 
-def find_winner(election: ElectionResult) -> str:
+    def get_copeland_score(self) -> Dict[str, int]:
+        votes = self.get_pairwise_votes()
+        return {
+            r1: len([r2 for r2 in self.routes if votes[r1][r2] > votes[r2][r1]]) -
+                len([r2 for r2 in self.routes if votes[r1][r2] < votes[r2][r1]]) for r1 in self.routes
+        }
+
+
+def find_irv_winner(election: ElectionResult) -> str:
     # finds winning route by using instant run-off voting
     print('Finding winning route.. starting score:\n{}'.format(election.get_interim_score()))
     while election.get_winner() is None:
@@ -104,6 +125,62 @@ def find_winner(election: ElectionResult) -> str:
 
     print('FOUND WINNING ROUTE!')
     winner = election.get_winner()
+    print(winner + ' got the most votes!')
+
+    return winner
+
+
+def get_smith_set(election: ElectionResult) -> Set[str]:
+    votes = election.get_pairwise_votes()
+    smith_relation = {
+        r1: {r2: r1 != r2 and votes[r1][r2] >= votes[r2][r1] for r2 in election.routes}
+        for r1 in election.routes
+    }
+    smith_closure = get_transitive_closure(smith_relation)
+    # now smith_closure[route1][route2] is true if there is a sequence of routes r, such that
+    # for each two subsequent elements r1, r2 of [route1] ++ r ++ [route2], r1 beats or ties with r2
+
+    # now extract the maximal elements from this
+    return {
+        r1 for r1 in election.routes if (
+            all([(r1 == r2 or smith_closure[r1][r2] or not smith_closure[r2][r1]) for r2 in election.routes])
+        )  # r1 is maximal if for all other r2, either r1 >= r2, or not (r2 >= r1)
+    }
+
+
+def get_transitive_closure(twod_dict: Dict[str, Dict[str, int]]) -> Dict[str, Dict[str, int]]:
+    # implements boolean Floyd-Warshall algorithm
+    keys = twod_dict.keys()
+    tc = copy.deepcopy(twod_dict)
+    for key1 in keys:
+        for key2 in keys:
+            for key3 in keys:
+                tc[key1][key2] |= tc[key1][key3] and tc[key3][key2]
+    return tc
+
+
+def find_tideman_winner(election: ElectionResult) -> str:
+    # finds winning route by using https://en.wikipedia.org/wiki/Tideman_alternative_method
+    print('Finding winning route.. starting score:\n{}'.format(election.get_interim_score()))
+    winner = None
+    while winner is None:
+        smith_set = get_smith_set(election)
+        if len(smith_set) == 1:
+            winner = next(iter(smith_set))
+            print('Most voters prefer one route over the others...')
+            continue
+        # eliminate all routes outside smith set
+        for route in election.routes:
+            if route not in smith_set:
+                print("Eliminating route '{}'".format(route))
+                election = election.without_route(route)
+        last_place = election.find_last_place()
+        print("Eliminating route '{}'".format(last_place))
+        election = election.without_route(last_place)  # returns new ElectionResult, does not modify in place
+        print('New interim score:\n{}'.format(election.get_interim_score()))
+        # TODO: add os.sleep(10) here..?
+
+    print('FOUND WINNING ROUTE!')
     print(winner + ' got the most votes!')
 
     return winner
--- test_election.py
+++ test_election.py
@@ -2,7 +2,7 @@ from unittest import TestCase
 from random import choices, sample
 import string
 
-from election import ElectionResult, find_winner
+from election import ElectionResult, find_irv_winner, get_smith_set, find_tideman_winner
 from voter import Voter
 
 
@@ -17,18 +17,18 @@ class TestElectionResult(TestCase):
         routes = ['r{}'.format(i) for i in range(1, num_routes + 1)]
         self.test_election = ElectionResult(
             routes, [
-                Voter('Ike', ['r1', 'r6', 'r2', 'r3', 'r5']),
-                Voter('Jochem', ['r3', 'r4', 'r6', 'r1', 'r2']),
-                Voter('Jesse', ['r2', 'r1', 'r5', 'r3', 'r4']),
-                Voter('Piotr', ['r3', 'r5', 'r2', 'r4', 'r7']),
-                Voter('Melina', ['r4', 'r1', 'r5', 'r6', 'r3']),
+                Voter('Ike', ['r1', 'r6', 'r2', 'r3', 'r5', 'r4', 'r7']),
+                Voter('Jochem', ['r3', 'r4', 'r6', 'r1', 'r2', 'r7', 'r5']),
+                Voter('Jesse', ['r2', 'r1', 'r5', 'r3', 'r4', 'r7', 'r6']),
+                Voter('Piotr', ['r3', 'r5', 'r2', 'r4', 'r7', 'r1', 'r6']),
+                Voter('Melina', ['r4', 'r1', 'r5', 'r6', 'r3', 'r2', 'r7']),
             ]
         )
 
     def test_get_winner(self):
         self.assertIsNone(self.test_election.get_winner())
         self.assertIsNone(self.test_election.without_route('r2').get_winner())
-        self.assertEquals(self.test_election.without_route('r2').without_route('r4').get_winner(), 'r1')
+        self.assertEqual(self.test_election.without_route('r2').without_route('r4').get_winner(), 'r1')
 
     def test_rank_routes(self):
         self.assertDictEqual(self.test_election.rank_routes(), {
@@ -48,7 +48,26 @@ class TestElectionResult(TestCase):
         # check that test_election is unchanged
         self.test_rank_routes()
         # check that voter rankings are changed correctly : original votes, with r2 removed
-        self.assertListEqual(election_without.voters[3].ranking, ['r3', 'r5', 'r4', 'r7'])
+        self.assertListEqual(election_without.voters[3].ranking, ['r3', 'r5', 'r4', 'r7', 'r1', 'r6'])
 
     def test_find_winner(self):
-        self.assertEquals(find_winner(self.test_election), 'r1')
+        self.assertEqual(find_irv_winner(self.test_election), 'r1')
+
+    def test_tideman_winner(self):
+        self.assertEqual(find_tideman_winner(self.test_election), 'r1')
+
+    def test_pairwise_votes(self):
+        votes = self.test_election.get_pairwise_votes()
+        self.assertGreater(votes['r1']['r3'], votes['r3']['r1'])
+        self.assertGreater(votes['r4']['r1'], votes['r1']['r4'])
+        self.assertGreater(votes['r3']['r4'], votes['r4']['r3'])
+
+    def test_smith_set(self):
+        # We have r4 > r1, r3 > r4. and additionally, r2 > r4 > r1 > r2
+        # Also r6 > r2 and r5 > r4, thus
+        self.assertSetEqual(get_smith_set(self.test_election), {'r1', 'r2', 'r3', 'r4', 'r5', 'r6'})
+        removed_some = self.test_election.without_route('r7').without_route('r6').without_route('r5')
+        self.assertEqual(get_smith_set(removed_some), {'r1', 'r2', 'r3', 'r4'})
+        # remaining choice order does not matter: r1 will win
+        self.assertEqual(get_smith_set(removed_some.without_route('r2').without_route('r4')), {'r1'})
+        self.assertEqual(get_smith_set(removed_some.without_route('r4').without_route('r2')), {'r1'})
